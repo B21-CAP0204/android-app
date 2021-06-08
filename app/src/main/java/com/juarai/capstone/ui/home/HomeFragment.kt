18fd27878a5b9919471fbec28ea89898937281f8
Unero <20809454+un-ro@users.noreply.github.com>
2021-06-08 13:00:29 +0700

    :lipstick: add animation and change Home UI

--- app/src/main/java/com/juarai/capstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/juarai/capstone/ui/home/HomeFragment.kt
@@ -13,26 +13,35 @@ import org.koin.android.ext.android.inject
 class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by inject()
-    private lateinit var _binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return _binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        viewModel.getUser().observe(viewLifecycleOwner, {
-//            if (it.isEmpty())
-//                println("Data masih kosong")
-//            else
-//                println(it)
-//        })
-        _binding.fabRegister.setOnClickListener {
+        viewModel.getUser().observe(viewLifecycleOwner, {
+            if (it.isEmpty())
+                println("Data masih kosong")
+            else
+                hideOther()
+        })
+
+        binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_registerFragment)
+        }
+    }
+
+    private fun hideOther() {
+        with(binding) {
+            lottieRegister.visibility = View.GONE
+            btnRegister.visibility = View.GONE
         }
     }
 }
