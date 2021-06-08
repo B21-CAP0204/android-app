ed54598ff985170d6d8f47dc3b4a6cae60bd6332
Unero <20809454+un-ro@users.noreply.github.com>
2021-06-08 13:02:51 +0700

    :lipstick: Validation Screen

--- app/src/main/java/com/juarai/capstone/ui/validation/ValidationFragment.kt
+++ app/src/main/java/com/juarai/capstone/ui/validation/ValidationFragment.kt
@@ -1,22 +1,48 @@
 package com.juarai.capstone.ui.validation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.juarai.capstone.R
+import androidx.navigation.fragment.findNavController
+import com.juarai.capstone.databinding.FragmentValidationBinding
+import es.dmoral.toasty.Toasty
 
 class ValidationFragment : Fragment() {
 
     val viewModel: ValidationViewModel by viewModels()
 
+    private var _binding: FragmentValidationBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_validation, container, false)
+    ): View {
+        _binding = FragmentValidationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lblKK.editText?.setText("123123123123")
+
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val action = ValidationFragmentDirections.actionValidationFragmentToHomeFragment()
+        with(binding) {
+            btnAcc.setOnClickListener {
+                Toasty.success(requireContext(), "Daftar Sukses", Toasty.LENGTH_SHORT).show()
+                findNavController().navigate(action)
+            }
+            btnCancel.setOnClickListener {
+                Toasty.error(requireContext(), "Daftar Dibatalkan", Toasty.LENGTH_SHORT).show()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
