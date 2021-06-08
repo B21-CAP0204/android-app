f1dc94c22d521b3351ee30f764f716eb2309b625
Unero <20809454+un-ro@users.noreply.github.com>
2021-06-08 13:03:31 +0700

    Change room module

--- app/src/main/java/com/juarai/capstone/di/Modules.kt
+++ app/src/main/java/com/juarai/capstone/di/Modules.kt
@@ -1,9 +1,11 @@
 package com.juarai.capstone.di
 
 import androidx.room.Room
+import com.juarai.capstone.BuildConfig
 import com.juarai.capstone.data.IRepository
 import com.juarai.capstone.data.Repository
 import com.juarai.capstone.data.local.AppDatabase
+import com.juarai.capstone.data.local.UserDao
 import com.juarai.capstone.data.network.Constants.BASE_URL
 import com.juarai.capstone.data.network.Endpoint
 import com.juarai.capstone.ui.camera.CameraViewModel
@@ -16,8 +18,9 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
+// Local Data
 val roomModule = module {
-    factory { get<AppDatabase>().userDao() }
     single {
         Room.databaseBuilder(
             androidContext(),
@@ -25,12 +28,19 @@ val roomModule = module {
             "juarai.db"
         ).fallbackToDestructiveMigration().build()
     }
+
+    fun provideDao(database: AppDatabase): UserDao {
+        return database.userDao()
+    }
+
+    single { provideDao(get()) }
 }
 
+// Network
 val retrofitModule = module {
     single {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(get())
             .build()
@@ -38,12 +48,14 @@ val retrofitModule = module {
     }
 }
 
+// Repository
 val repositoryModule = module {
-    single<IRepository> {
+    single {
         Repository(get())
     }
 }
 
+// View Model
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { RegisterViewModel(get()) }
